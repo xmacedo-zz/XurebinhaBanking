d26ad754bfcc033eae525449b16964bdad16955e
macedo <felipe.x.macedo@gmail.com>
2020-03-06 16:30:49 -0300

    Removing intelliJ files

---
+++ src/main/java/br/com/NakedBanking/NakedBankingApplication.java
@@ -0,0 +1,13 @@
+package br.com.NakedBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NakedBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NakedBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/NakedBanking/NakedBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.NakedBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NakedBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
