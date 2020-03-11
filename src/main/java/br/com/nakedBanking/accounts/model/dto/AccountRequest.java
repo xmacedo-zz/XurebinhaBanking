959a030037d79ce050fc68ef43fe65a530676be7
Matheus <matheus.abreu@southsystem.com.br>
2020-03-11 10:41:03 -0300

    add annotations in request

--- src/main/java/br/com/nakedBanking/accounts/model/dto/AccountRequest.java
+++ src/main/java/br/com/nakedBanking/accounts/model/dto/AccountRequest.java
@@ -1,14 +1,24 @@
 package br.com.nakedBanking.accounts.model.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class AccountRequest {
 
     @NotBlank
+    @Pattern(regexp="[0-9]+", message = "just enter numbers")
+    @Size(min = 6, max = 6, message = "numero must be 6 digits")
     private String number;
+
     @NotBlank
+    @Pattern(regexp="[0-9]+", message = "just enter numbers")
+    @Size(min = 4, max = 4, message = "agencia must be 4 digits")
     private String agency;
+
     @NotBlank
+    @Pattern(regexp="[0-9]+", message = "just enter numbers")
+    @Size(min = 11, max = 11, message = "cpf must be 11 digits")
     private String cpf;
     private Boolean status;
 
