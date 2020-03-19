f8d200171d222d44e5ce3ddbf27a4962a505e895
Felipe Macedo <felipe.x.macedo@gmail.com>
2020-03-19 20:22:08 -0300

    Adjust name of project

--- backend/src/main/java/br/com/xurebinhaBanking/XurebinhaBankingApplication.java
+++ backend/src/main/java/br/com/xurebinhaBanking/XurebinhaBankingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NakedBankingApplication {
+public class XurebinhaBankingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NakedBankingApplication.class, args);
+		SpringApplication.run(XurebinhaBankingApplication.class, args);
 	}
 
 }
